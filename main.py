690c1517c7b1d3dab3818c8f1e1649c5caa2c98d
Tapandat <166542132+Tapandat@users.noreply.github.com>
2026-04-22 11:50:20 +0530

    Import data generation and numpy functions

--- main.py
+++ main.py
@@ -1,1 +1,15 @@
+from src.generate_data import generate_cricket_data
+from src.numpy_advanced import *
 
+players, runs, balls = generate_cricket_data()
+
+print("Players:", players)
+print("Runs:", runs)
+
+print("\nMemory Optimization:", memory_optimization())
+
+print("\nString Ops:", string_operations(players))
+
+print("\nContiguous Arrays:", contiguous_arrays())
+
+print("\nIteration:", iteration_example(runs))
