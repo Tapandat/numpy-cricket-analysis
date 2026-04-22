a0beced4707f946671fec1afebb5191fa162a188
Tapandat <166542132+Tapandat@users.noreply.github.com>
2026-04-22 11:47:45 +0530

    Add function to generate cricket player data

--- src/generate_data.py
+++ src/generate_data.py
@@ -1,1 +1,8 @@
+import numpy as np
 
+def generate_cricket_data():
+    players = np.array(['virat', 'rohit', 'dhoni', 'rahul'])
+    runs = np.random.randint(0, 100, size=4, dtype=np.int16)
+    balls = np.random.randint(1, 60, size=4, dtype=np.int16)
+
+    return players, runs, balls
