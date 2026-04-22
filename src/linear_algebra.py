2bea3c46c619e51113a1d65545d71bd35280994a
Tapandat <166542132+Tapandat@users.noreply.github.com>
2026-04-22 12:22:16 +0530

    Implement linear algebra demo function
    Added a demo function for linear algebra operations using NumPy.

--- src/linear_algebra.py
+++ src/linear_algebra.py
@@ -1,1 +1,19 @@
+import numpy as np
 
+def linear_algebra_demo():
+    a = np.array([1, 2, 3])
+    b = np.array([4, 5, 6])
+
+    dot_product = np.dot(a, b)
+
+    A = np.array([[1, 2],
+                  [3, 4]])
+
+    B = np.array([[5, 6],
+                  [7, 8]])
+
+    matrix_product = np.matmul(A, B)
+    transpose_A = A.T
+    determinant_A = np.linalg.det(A)
+
+    return dot_product, matrix_product, transpose_A, determinant_A
